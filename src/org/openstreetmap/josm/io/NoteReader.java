SamB/josm

commit 100b17f7c9397d413fd97561d899735e7c8d472d

sonar, javadoc
git-svn-id: https://josm.openstreetmap.de/svn/trunk@10134 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/io/NoteReader.java
@@ -100,9 +100,9 @@ public class NoteReader {
                 thisNote.setId(Long.parseLong(attrs.getValue("id")));
                 String closedTimeStr = attrs.getValue("closed_at");
                 if (closedTimeStr == null) { //no closed_at means the note is still open
-                    thisNote.setState(Note.State.open);
+                    thisNote.setState(Note.State.OPEN);
                 } else {
-                    thisNote.setState(Note.State.closed);
+                    thisNote.setState(Note.State.CLOSED);
                     thisNote.setClosedAt(DateUtils.fromString(closedTimeStr));
                 }
                 thisNote.setCreatedAt(DateUtils.fromString(attrs.getValue("created_at")));

src/org/openstreetmap/josm/actions/relation/RecentRelationsAction.java
@@ -158,7 +158,7 @@ public class RecentRelationsAction implements ActionListener, CommandQueueListen
             }
         }
 
-        public static void launch(Component parent, KeyStroke keystroke) {
+        protected static void launch(Component parent, KeyStroke keystroke) {
             Rectangle r = parent.getBounds();
             new RecentRelationsPopupMenu(getRecentRelationsOnActiveLayer(), keystroke).show(parent, r.x, r.y + r.height);
         }

src/org/openstreetmap/josm/actions/upload/UploadNotesTask.java
@@ -88,25 +88,25 @@ public class UploadNotesTask {
                 }
                 Note newNote;
                 switch (comment.getNoteAction()) {
-                case opened:
+                case OPENED:
                     if (Main.isDebugEnabled()) {
                         Main.debug("opening new note");
                     }
                     newNote = api.createNote(note.getLatLon(), comment.getText(), monitor);
                     break;
-                case closed:
+                case CLOSED:
                     if (Main.isDebugEnabled()) {
                         Main.debug("closing note " + note.getId());
                     }
                     newNote = api.closeNote(note, comment.getText(), monitor);
                     break;
-                case commented:
+                case COMMENTED:
                     if (Main.isDebugEnabled()) {
                         Main.debug("adding comment to note " + note.getId());
                     }
                     newNote = api.addCommentToNote(note, comment.getText(), monitor);
                     break;
-                case reopened:
+                case REOPENED:
                     if (Main.isDebugEnabled()) {
                         Main.debug("reopening note " + note.getId());
                     }

src/org/openstreetmap/josm/data/cache/ICachedLoaderListener.java
@@ -1,6 +1,10 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.data.cache;
 
+/**
+ * Cache loader listener.
+ * @since 8168
+ */
 public interface ICachedLoaderListener {
 
     /**

src/org/openstreetmap/josm/data/cache/JCSCachedTileLoaderJob.java
@@ -141,6 +141,7 @@ public abstract class JCSCachedTileLoaderJob<K, V extends CacheEntry> implements
         }
     }
 
+    @Override
     public V get() {
         ensureCacheElement();
         return cacheData;

src/org/openstreetmap/josm/data/notes/Note.java
@@ -11,12 +11,18 @@ import java.util.Objects;
 import org.openstreetmap.josm.data.coor.LatLon;
 
 /**
- * A map note. It always has at least one comment since a comment is required
- * to create a note on osm.org
+ * A map note. It always has at least one comment since a comment is required to create a note on osm.org.
+ * @since 7451
  */
 public class Note {
 
-    public enum State { open, closed }
+    /** Note state */
+    public enum State {
+        /** Note is open */
+        OPEN,
+        /** Note is closed */
+        CLOSED
+    }
 
     private long id;
     private LatLon latLon;
@@ -38,6 +44,10 @@ public class Note {
         return id;
     }
 
+    /**
+     * Sets note id.
+     * @param id OSM ID of this note
+     */
     public void setId(long id) {
         this.id = id;
     }
@@ -52,6 +62,10 @@ public class Note {
         return createdAt;
     }
 
+    /**
+     * Sets date at which this note has been created.
+     * @param createdAt date at which this note has been created
+     */
     public void setCreatedAt(Date createdAt) {
         this.createdAt = createdAt;
     }
@@ -61,6 +75,10 @@ public class Note {
         return closedAt;
     }
 
+    /**
+     * Sets date at which this note has been closed.
+     * @param closedAt date at which this note has been closed
+     */
     public void setClosedAt(Date closedAt) {
         this.closedAt = closedAt;
     }
@@ -70,6 +88,10 @@ public class Note {
         return state;
     }
 
+    /**
+     * Sets the note state.
+     * @param state note state (open or closed)
+     */
     public void setState(State state) {
         this.state = state;
     }
@@ -79,6 +101,10 @@ public class Note {
         return comments;
     }
 
+    /**
+     * Adds a comment.
+     * @param comment note comment
+     */
     public void addComment(NoteComment comment) {
         comments.add(comment);
     }
@@ -111,8 +137,10 @@ public class Note {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
         Note note = (Note) obj;
         return id == note.id;
     }

src/org/openstreetmap/josm/data/notes/NoteComment.java
@@ -8,6 +8,7 @@ import org.openstreetmap.josm.data.osm.User;
 /**
  * Represents a comment made on a note. All notes have at least on comment
  * which is the comment the note was opened with. Comments are immutable.
+ * @since 7451
  */
 public class NoteComment {
 
@@ -24,11 +25,16 @@ public class NoteComment {
      * while others indicate the note being opened, closed or reopened
      */
     public enum Action {
-        opened,
-        closed,
-        reopened,
-        commented,
-        hidden
+        /** note has been opened */
+        OPENED,
+        /** note has been closed */
+        CLOSED,
+        /** note has been reopened */
+        REOPENED,
+        /** note has been commented */
+        COMMENTED,
+        /** note has been hidden */
+        HIDDEN
     }
 
     /**

src/org/openstreetmap/josm/data/notes/package-info.java
@@ -1,0 +1,6 @@
+// License: GPL. For details, see LICENSE file.
+
+/**
+ * Provides the classes for OSM notes. See <a href="https://wiki.openstreetmap.org/wiki/Notes">Notes</a>.
+ */
+package org.openstreetmap.josm.data.notes;

commit 04d69701e2e8938601024683dea052d625992150

fix note parsing broken by r10134
git-svn-id: https://josm.openstreetmap.de/svn/trunk@10136 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/io/NoteReader.java
@@ -8,6 +8,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -115,7 +116,7 @@ public class NoteReader {
                     commentUid = Long.parseLong(uidStr);
                 }
                 commentUsername = attrs.getValue("user");
-                noteAction = Action.valueOf(attrs.getValue("action"));
+                noteAction = Action.valueOf(attrs.getValue("action").toUpperCase(Locale.ENGLISH));
                 commentCreateDate = DateUtils.fromString(attrs.getValue("timestamp"));
                 String isNew = attrs.getValue("is_new");
                 if (isNew == null) {
@@ -160,7 +161,7 @@ public class NoteReader {
                 thisNote.setId(Long.parseLong(buffer.toString()));
                 break;
             case "status":
-                thisNote.setState(Note.State.valueOf(buffer.toString()));
+                thisNote.setState(Note.State.valueOf(buffer.toString().toUpperCase(Locale.ENGLISH)));
                 break;
             case "date_created":
                 thisNote.setCreatedAt(DateUtils.fromString(buffer.toString()));
@@ -182,7 +183,7 @@ public class NoteReader {
                 buffer.setLength(0);
                 break;
             case "action":
-                noteAction = Action.valueOf(buffer.toString());
+                noteAction = Action.valueOf(buffer.toString().toUpperCase(Locale.ENGLISH));
                 break;
             case "note": //nothing to do for comment or note, already handled above
             case "comment":