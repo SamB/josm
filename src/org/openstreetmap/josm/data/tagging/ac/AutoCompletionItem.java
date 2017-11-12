SamB/josm

commit 7bed838493c6426e9b1db06d7ba43013ca512cd7

see #15229 - see #15182 - deprecate non-GUI `AutoCompletion*` classes from `gui.tagging.ac`. Offer better replacements in new `data.tagging.ac` package
git-svn-id: https://josm.openstreetmap.de/svn/trunk@12859 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/data/tagging/ac/AutoCompletionItem.java
@@ -1,0 +1,140 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.data.tagging.ac;
+
+import org.openstreetmap.josm.tools.CheckParameterUtil;
+
+/**
+ * Represents an entry in the set of auto completion values.
+ *
+ *  An AutoCompletionItem has a <em>priority</em> and a <em>value</em>.
+ *
+ *  The priority helps to sort the auto completion items according to their importance. For instance,
+ *  in an auto completion set for tag names, standard tag names would be assigned a higher
+ *  priority than arbitrary tag names present in the current data set. There are three priority levels,
+ *  {@link AutoCompletionPriority}.
+ *
+ * The value is a string which will be displayed in the auto completion list.
+ * @since 12859 (copied from {@code gui.tagging.ac.AutoCompletionListItem})
+ */
+public class AutoCompletionItem implements Comparable<AutoCompletionItem> {
+
+    /** the priority of this item */
+    private AutoCompletionPriority priority;
+    /** the value of this item */
+    private String value;
+
+    /**
+     * Constructs a new {@code AutoCompletionItem} with the given value and priority.
+     * @param value The value
+     * @param priority The priority
+     */
+    public AutoCompletionItem(String value, AutoCompletionPriority priority) {
+        this.value = value;
+        this.priority = priority;
+    }
+
+    /**
+     * Constructs a new {@code AutoCompletionItem} with the given value and unknown priority.
+     * @param value The value
+     */
+    public AutoCompletionItem(String value) {
+        this.value = value;
+        priority = AutoCompletionPriority.UNKNOWN;
+    }
+
+    /**
+     * Constructs a new {@code AutoCompletionItem}.
+     */
+    public AutoCompletionItem() {
+        value = "";
+        priority = AutoCompletionPriority.UNKNOWN;
+    }
+
+    /**
+     * Returns the priority.
+     * @return the priority
+     */
+    public AutoCompletionPriority getPriority() {
+        return priority;
+    }
+
+    /**
+     * Sets the priority.
+     * @param priority  the priority
+     */
+    public void setPriority(AutoCompletionPriority priority) {
+        this.priority = priority;
+    }
+
+    /**
+     * Returns the value.
+     * @return the value
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * sets the value
+     * @param value the value; must not be null
+     * @throws IllegalArgumentException if value if null
+     */
+    public void setValue(String value) {
+        CheckParameterUtil.ensureParameterNotNull(value, "value");
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<val='")
+          .append(value)
+          .append("',")
+          .append(priority)
+          .append('>');
+        return sb.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((priority == null) ? 0 : priority.hashCode());
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (obj instanceof String)
+            return obj.equals(value);
+        if (getClass() != obj.getClass())
+            return false;
+        final AutoCompletionItem other = (AutoCompletionItem) obj;
+        if (priority == null) {
+            if (other.priority != null)
+                return false;
+        } else if (!priority.equals(other.priority))
+            return false;
+        if (value == null) {
+            if (other.value != null)
+                return false;
+        } else if (!value.equals(other.value))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int compareTo(AutoCompletionItem other) {
+        int ret = other.priority.compareTo(priority); // higher priority items come first in the list
+        if (ret != 0)
+            return ret;
+        else
+            return this.value.compareTo(other.value);
+    }
+}

src/org/openstreetmap/josm/data/tagging/ac/AutoCompletionSet.java
@@ -1,0 +1,86 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.data.tagging.ac;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+/**
+ * A sorted set of {@link AutoCompletionItem}s.
+ *
+ * Items are sorted with higher priority first, then according to lexicographic order
+ * on the value of the {@code AutoCompletionListItem}.
+ *
+ * @since 12859 (extracted from {@code gui.tagging.ac.AutoCompletionList})
+ */
+public class AutoCompletionSet extends TreeSet<AutoCompletionItem> {
+
+    @Override
+    public boolean add(AutoCompletionItem e) {
+        // Is there already an item for the value?
+        Optional<AutoCompletionItem> result = stream().filter(i -> i.getValue().equals(e.getValue())).findFirst();
+        if (result.isPresent()) {
+            AutoCompletionItem item = result.get();
+            // yes: merge priorities
+            AutoCompletionPriority newPriority = item.getPriority().mergeWith(e.getPriority());
+            // if needed, remove/re-add the updated item to maintain set ordering
+            if (!item.getPriority().equals(newPriority)) {
+                remove(item);
+                item.setPriority(newPriority);
+                return add(item);
+            } else {
+                return false;
+            }
+        } else {
+            return super.add(e);
+        }
+    }
+
+    /**
+     * Adds a list of strings to this list. Only strings which
+     * are not null and which do not exist yet in the list are added.
+     *
+     * @param values a list of strings to add
+     * @param priority the priority to use
+     * @return {@code true} if this set changed as a result of the call
+     */
+    public boolean addAll(Collection<String> values, AutoCompletionPriority priority) {
+        return addAll(values.stream().filter(v -> v != null).map(v -> new AutoCompletionItem(v, priority)).collect(Collectors.toList()));
+    }
+
+    /**
+     * Adds values that have been entered by the user.
+     * @param values values that have been entered by the user
+     * @return {@code true} if this set changed as a result of the call
+     */
+    public boolean addUserInput(Collection<String> values) {
+        int i = 0;
+        boolean modified = false;
+        for (String value : values) {
+            if (value != null && add(new AutoCompletionItem(value, new AutoCompletionPriority(false, false, false, i++)))) {
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+    /**
+     * Checks whether an item with the given value is already in the list. Ignores priority of the items.
+     *
+     * @param value the value of an auto completion item
+     * @return true, if value is in the list; false, otherwise
+     */
+    public boolean contains(String value) {
+        return stream().anyMatch(i -> i.getValue().equals(value));
+    }
+
+    /**
+     * Removes the auto completion item with key <code>key</code>
+     * @param key the key
+     * @return {@code true} if an element was removed
+     */
+    public boolean remove(String key) {
+        return removeIf(i -> i.getValue().equals(key));
+    }
+}

src/org/openstreetmap/josm/gui/tagging/ac/AutoCompletionListItem.java
@@ -1,7 +1,7 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.gui.tagging.ac;
 
-import org.openstreetmap.josm.tools.CheckParameterUtil;
+import org.openstreetmap.josm.data.tagging.ac.AutoCompletionItem;
 
 /**
  * Represents an entry in the list of auto completion values.
@@ -14,14 +14,13 @@ import org.openstreetmap.josm.tools.CheckParameterUtil;
  *  {@link AutoCompletionItemPriority}.
  *
  * The value is a string which will be displayed in the auto completion list.
- *
+ * @deprecated To be removed end of 2017. Use {@link AutoCompletionItem} instead
  */
+@Deprecated
 public class AutoCompletionListItem implements Comparable<AutoCompletionListItem> {
 
-    /** the pritority of this item */
-    private AutoCompletionItemPriority priority;
-    /** the value of this item */
-    private String value;
+    /** the item */
+    private final AutoCompletionItem item;
 
     /**
      * Constructs a new {@code AutoCompletionListItem} with the given value and priority.
@@ -29,8 +28,7 @@ public class AutoCompletionListItem implements Comparable<AutoCompletionListItem
      * @param priority The priority
      */
     public AutoCompletionListItem(String value, AutoCompletionItemPriority priority) {
-        this.value = value;
-        this.priority = priority;
+        this.item = new AutoCompletionItem(value, priority.getPriority());
     }
 
     /**
@@ -38,16 +36,23 @@ public class AutoCompletionListItem implements Comparable<AutoCompletionListItem
      * @param value The value
      */
     public AutoCompletionListItem(String value) {
-        this.value = value;
-        priority = AutoCompletionItemPriority.UNKNOWN;
+        this.item = new AutoCompletionItem(value);
     }
 
     /**
      * Constructs a new {@code AutoCompletionListItem}.
      */
     public AutoCompletionListItem() {
-        value = "";
-        priority = AutoCompletionItemPriority.UNKNOWN;
+        this.item = new AutoCompletionItem();
+    }
+
+    /**
+     * Constructs a new {@code AutoCompletionListItem} from an existing {@link AutoCompletionItem}.
+     * @param other {@code AutoCompletionItem} to convert
+     * @since 12859
+     */
+    public AutoCompletionListItem(AutoCompletionItem other) {
+        this.item = other;
     }
 
     /**
@@ -55,7 +60,7 @@ public class AutoCompletionListItem implements Comparable<AutoCompletionListItem
      * @return the priority
      */
     public AutoCompletionItemPriority getPriority() {
-        return priority;
+        return new AutoCompletionItemPriority(item.getPriority());
     }
 
     /**
@@ -63,7 +68,7 @@ public class AutoCompletionListItem implements Comparable<AutoCompletionListItem
      * @param priority  the priority
      */
     public void setPriority(AutoCompletionItemPriority priority) {
-        this.priority = priority;
+        item.setPriority(priority.getPriority());
     }
 
     /**
@@ -71,7 +76,7 @@ public class AutoCompletionListItem implements Comparable<AutoCompletionListItem
      * @return the value
      */
     public String getValue() {
-        return value;
+        return item.getValue();
     }
 
     /**
@@ -80,29 +85,17 @@ public class AutoCompletionListItem implements Comparable<AutoCompletionListItem
      * @throws IllegalArgumentException if value if null
      */
     public void setValue(String value) {
-        CheckParameterUtil.ensureParameterNotNull(value, "value");
-        this.value = value;
+        item.setValue(value);
     }
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("<val='")
-          .append(value)
-          .append("',")
-          .append(priority)
-          .append('>');
-        return sb.toString();
+        return item.toString();
     }
 
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result
-                + ((priority == null) ? 0 : priority.hashCode());
-        result = prime * result + ((value == null) ? 0 : value.hashCode());
-        return result;
+        return item.hashCode();
     }
 
     @Override
@@ -112,29 +105,24 @@ public class AutoCompletionListItem implements Comparable<AutoCompletionListItem
         if (obj == null)
             return false;
         if (obj instanceof String)
-            return obj.equals(value);
+            return obj.equals(item.getValue());
         if (getClass() != obj.getClass())
             return false;
         final AutoCompletionListItem other = (AutoCompletionListItem) obj;
-        if (priority == null) {
-            if (other.priority != null)
-                return false;
-        } else if (!priority.equals(other.priority))
-            return false;
-        if (value == null) {
-            if (other.value != null)
-                return false;
-        } else if (!value.equals(other.value))
-            return false;
-        return true;
+        return item.equals(other.item);
     }
 
     @Override
     public int compareTo(AutoCompletionListItem other) {
-        int ret = other.priority.compareTo(priority); // higher priority items come first in the list
-        if (ret != 0)
-            return ret;
-        else
-            return this.value.compareTo(other.value);
+        return item.compareTo(other.item);
+    }
+
+    /**
+     * Returns the underlying item.
+     * @return the underlying item
+     * @since 12859
+     */
+    public AutoCompletionItem getItem() {
+        return item;
     }
 }

src/org/openstreetmap/josm/gui/tagging/ac/AutoCompletionManager.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
@@ -14,6 +15,7 @@ import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.Set;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
@@ -28,6 +30,9 @@ import org.openstreetmap.josm.data.osm.event.PrimitivesRemovedEvent;
 import org.openstreetmap.josm.data.osm.event.RelationMembersChangedEvent;
 import org.openstreetmap.josm.data.osm.event.TagsChangedEvent;
 import org.openstreetmap.josm.data.osm.event.WayNodesChangedEvent;
+import org.openstreetmap.josm.data.tagging.ac.AutoCompletionItem;
+import org.openstreetmap.josm.data.tagging.ac.AutoCompletionPriority;
+import org.openstreetmap.josm.data.tagging.ac.AutoCompletionSet;
 import org.openstreetmap.josm.gui.MainApplication;
 import org.openstreetmap.josm.gui.layer.LayerManager.LayerAddEvent;
 import org.openstreetmap.josm.gui.layer.LayerManager.LayerChangeListener;
@@ -45,11 +50,10 @@ import org.openstreetmap.josm.tools.Utils;
  * AutoCompletionManager holds a cache of keys with a list of
  * possible auto completion values for each key.
  *
- * Each DataSet is assigned one AutoCompletionManager instance such that
+ * Each DataSet can be assigned one AutoCompletionManager instance such that
  * <ol>
  *   <li>any key used in a tag in the data set is part of the key list in the cache</li>
- *   <li>any value used in a tag for a specific key is part of the autocompletion list of
- *     this key</li>
+ *   <li>any value used in a tag for a specific key is part of the autocompletion list of this key</li>
  * </ol>
  *
  * Building up auto completion lists should not
@@ -270,14 +274,13 @@ public class AutoCompletionManager implements DataSetListener {
     }
 
     /**
-     * Populates the {@link AutoCompletionList} with the currently cached
-     * member roles.
+     * Populates the {@link AutoCompletionList} with the currently cached member roles.
      *
      * @param list the list to populate
      */
     public void populateWithMemberRoles(AutoCompletionList list) {
-        list.add(TaggingPresets.getPresetRoles(), AutoCompletionItemPriority.IS_IN_STANDARD);
-        list.add(getRoleCache(), AutoCompletionItemPriority.IS_IN_DATASET);
+        list.add(TaggingPresets.getPresetRoles(), AutoCompletionPriority.IS_IN_STANDARD);
+        list.add(getRoleCache(), AutoCompletionPriority.IS_IN_DATASET);
     }
 
     /**
@@ -296,10 +299,10 @@ public class AutoCompletionManager implements DataSetListener {
         if (r != null && presets != null && !presets.isEmpty()) {
             for (TaggingPreset tp : presets) {
                 if (tp.roles != null) {
-                    list.add(Utils.transform(tp.roles.roles, (Function<Role, String>) x -> x.key), AutoCompletionItemPriority.IS_IN_STANDARD);
+                    list.add(Utils.transform(tp.roles.roles, (Function<Role, String>) x -> x.key), AutoCompletionPriority.IS_IN_STANDARD);
                 }
             }
-            list.add(r.getMemberRoles(), AutoCompletionItemPriority.IS_IN_DATASET);
+            list.add(r.getMemberRoles(), AutoCompletionPriority.IS_IN_DATASET);
         } else {
             populateWithMemberRoles(list);
         }
@@ -311,15 +314,14 @@ public class AutoCompletionManager implements DataSetListener {
      * @param list the list to populate
      */
     public void populateWithKeys(AutoCompletionList list) {
-        list.add(TaggingPresets.getPresetKeys(), AutoCompletionItemPriority.IS_IN_STANDARD);
-        list.add(new AutoCompletionListItem("source", AutoCompletionItemPriority.IS_IN_STANDARD));
-        list.add(getDataKeys(), AutoCompletionItemPriority.IS_IN_DATASET);
+        list.add(TaggingPresets.getPresetKeys(), AutoCompletionPriority.IS_IN_STANDARD);
+        list.add(new AutoCompletionItem("source", AutoCompletionPriority.IS_IN_STANDARD));
+        list.add(getDataKeys(), AutoCompletionPriority.IS_IN_DATASET);
         list.addUserInput(getUserInputKeys());
     }
 
     /**
-     * Populates the an {@link AutoCompletionList} with the currently cached
-     * values for a tag
+     * Populates the an {@link AutoCompletionList} with the currently cached values for a tag
      *
      * @param list the list to populate
      * @param key the tag key
@@ -329,16 +331,15 @@ public class AutoCompletionManager implements DataSetListener {
     }
 
     /**
-     * Populates the an {@link AutoCompletionList} with the currently cached
-     * values for some given tags
+     * Populates the an {@link AutoCompletionList} with the currently cached values for some given tags
      *
      * @param list the list to populate
      * @param keys the tag keys
      */
     public void populateWithTagValues(AutoCompletionList list, List<String> keys) {
         for (String key : keys) {
-            list.add(TaggingPresets.getPresetValues(key), AutoCompletionItemPriority.IS_IN_STANDARD);
-            list.add(getDataValues(key), AutoCompletionItemPriority.IS_IN_DATASET);
+            list.add(TaggingPresets.getPresetValues(key), AutoCompletionPriority.IS_IN_STANDARD);
+            list.add(getDataValues(key), AutoCompletionPriority.IS_IN_DATASET);
             list.addUserInput(getUserInputValues(key));
         }
     }
@@ -346,37 +347,110 @@ public class AutoCompletionManager implements DataSetListener {
     /**
      * Returns the currently cached tag keys.
      * @return a list of tag keys
+     * @deprecated to be removed end of 2017. Use {@link #getTagKeys()} instead
      */
+    @Deprecated
     public List<AutoCompletionListItem> getKeys() {
-        AutoCompletionList list = new AutoCompletionList();
-        populateWithKeys(list);
-        return list.getList();
+        return getTagKeys().stream().map(AutoCompletionListItem::new).collect(Collectors.toList());
     }
 
     /**
      * Returns the currently cached tag values for a given tag key.
      * @param key the tag key
      * @return a list of tag values
+     * @deprecated to be removed end of 2017. Use {@link #getTagValues(String)} instead
      */
+    @Deprecated
     public List<AutoCompletionListItem> getValues(String key) {
-        return getValues(Arrays.asList(key));
+        return getTagValues(key).stream().map(AutoCompletionListItem::new).collect(Collectors.toList());
     }
 
     /**
      * Returns the currently cached tag values for a given list of tag keys.
      * @param keys the tag keys
      * @return a list of tag values
+     * @deprecated to be removed end of 2017. Use {@link #getTagValues(List)} instead
      */
+    @Deprecated
     public List<AutoCompletionListItem> getValues(List<String> keys) {
-        AutoCompletionList list = new AutoCompletionList();
-        populateWithTagValues(list, keys);
-        return list.getList();
+        return getTagValues(keys).stream().map(AutoCompletionListItem::new).collect(Collectors.toList());
     }
 
-    /*********************************************************
+    private static List<AutoCompletionItem> setToList(AutoCompletionSet set, Comparator<AutoCompletionItem> comparator) {
+        List<AutoCompletionItem> list = set.stream().collect(Collectors.toList());
+        list.sort(comparator);
+        return list;
+    }
+
+    /**
+     * Returns the currently cached tag keys.
+     * @return a set of tag keys
+     * @since 12859
+     */
+    public AutoCompletionSet getTagKeys() {
+        AutoCompletionList list = new AutoCompletionList();
+        populateWithKeys(list);
+        return list.getSet();
+    }
+
+    /**
+     * Returns the currently cached tag keys.
+     * @param comparator the custom comparator used to sort the list
+     * @return a list of tag keys
+     * @since 12859
+     */
+    public List<AutoCompletionItem> getTagKeys(Comparator<AutoCompletionItem> comparator) {
+        return setToList(getTagKeys(), comparator);
+    }
+
+    /**
+     * Returns the currently cached tag values for a given tag key.
+     * @param key the tag key
+     * @return a set of tag values
+     * @since 12859
+     */
+    public AutoCompletionSet getTagValues(String key) {
+        return getTagValues(Arrays.asList(key));
+    }
+
+    /**
+     * Returns the currently cached tag values for a given tag key.
+     * @param key the tag key
+     * @param comparator the custom comparator used to sort the list
+     * @return a list of tag values
+     * @since 12859
+     */
+    public List<AutoCompletionItem> getTagValues(String key, Comparator<AutoCompletionItem> comparator) {
+        return setToList(getTagValues(key), comparator);
+    }
+
+    /**
+     * Returns the currently cached tag values for a given list of tag keys.
+     * @param keys the tag keys
+     * @return a set of tag values
+     * @since 12859
+     */
+    public AutoCompletionSet getTagValues(List<String> keys) {
+        AutoCompletionList list = new AutoCompletionList();
+        populateWithTagValues(list, keys);
+        return list.getSet();
+    }
+
+    /**
+     * Returns the currently cached tag values for a given list of tag keys.
+     * @param keys the tag keys
+     * @param comparator the custom comparator used to sort the list
+     * @return a set of tag values
+     * @since 12859
+     */
+    public List<AutoCompletionItem> getTagValues(List<String> keys, Comparator<AutoCompletionItem> comparator) {
+        return setToList(getTagValues(keys), comparator);
+    }
+
+    /*
      * Implementation of the DataSetListener interface
      *
-     **/
+     */
 
     @Override
     public void primitivesAdded(PrimitivesAddedEvent event) {

commit 1aed062325263b805acc192026bc65faa39b8f0c

fix #15547 - fix major performance drawback of `AutoCompletionSet` (regression from r12859)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@13121 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/data/tagging/ac/AutoCompletionItem.java
@@ -21,7 +21,7 @@ public class AutoCompletionItem implements Comparable<AutoCompletionItem> {
     /** the priority of this item */
     private AutoCompletionPriority priority;
     /** the value of this item */
-    private String value;
+    private final String value;
 
     /**
      * Constructs a new {@code AutoCompletionItem} with the given value and priority.
@@ -78,10 +78,12 @@ public class AutoCompletionItem implements Comparable<AutoCompletionItem> {
      * sets the value
      * @param value the value; must not be null
      * @throws IllegalArgumentException if value if null
+     * @deprecated value is now final, set it when constructing the object
      */
+    @Deprecated
     public void setValue(String value) {
         CheckParameterUtil.ensureParameterNotNull(value, "value");
-        this.value = value;
+        throw new UnsupportedOperationException("setValue() is no longer supported");
     }
 
     @Override

src/org/openstreetmap/josm/data/tagging/ac/AutoCompletionSet.java
@@ -4,6 +4,7 @@ package org.openstreetmap.josm.data.tagging.ac;
 import java.util.Collection;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 
@@ -17,25 +18,49 @@ import java.util.stream.Collectors;
  */
 public class AutoCompletionSet extends TreeSet<AutoCompletionItem> {
 
+    // Keep a separate tree set of values for determining fast if a value is present
+    private final Set<String> values = new TreeSet<>();
+
     @Override
     public boolean add(AutoCompletionItem e) {
         // Is there already an item for the value?
-        Optional<AutoCompletionItem> result = stream().filter(i -> i.getValue().equals(e.getValue())).findFirst();
-        if (result.isPresent()) {
-            AutoCompletionItem item = result.get();
-            // yes: merge priorities
-            AutoCompletionPriority newPriority = item.getPriority().mergeWith(e.getPriority());
-            // if needed, remove/re-add the updated item to maintain set ordering
-            if (!item.getPriority().equals(newPriority)) {
-                remove(item);
-                item.setPriority(newPriority);
-                return add(item);
+        String value = e.getValue();
+        if (contains(value)) { // Fast
+            Optional<AutoCompletionItem> result = stream().filter(i -> i.getValue().equals(e.getValue())).findFirst(); // Slow
+            if (result.isPresent()) {
+                AutoCompletionItem item = result.get();
+                // yes: merge priorities
+                AutoCompletionPriority newPriority = item.getPriority().mergeWith(e.getPriority());
+                // if needed, remove/re-add the updated item to maintain set ordering
+                if (!item.getPriority().equals(newPriority)) {
+                    super.remove(item);
+                    item.setPriority(newPriority);
+                    return super.add(item);
+                } else {
+                    return false;
+                }
             } else {
-                return false;
+                // Should never happen if values is correctly synchronized with this set
+                throw new IllegalStateException(value);
             }
         } else {
+            values.add(value);
             return super.add(e);
         }
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if (o instanceof AutoCompletionItem) {
+            values.remove(((AutoCompletionItem) o).getValue());
+        }
+        return super.remove(o);
+    }
+
+    @Override
+    public void clear() {
+        values.clear();
+        super.clear();
     }
 
     /**
@@ -73,7 +98,7 @@ public class AutoCompletionSet extends TreeSet<AutoCompletionItem> {
      * @return true, if value is in the list; false, otherwise
      */
     public boolean contains(String value) {
-        return stream().anyMatch(i -> i.getValue().equals(value));
+        return values.contains(value);
     }
 
     /**
@@ -82,6 +107,6 @@ public class AutoCompletionSet extends TreeSet<AutoCompletionItem> {
      * @return {@code true} if an element was removed
      */
     public boolean remove(String key) {
-        return removeIf(i -> i.getValue().equals(key));
+        return values.remove(key) && removeIf(i -> i.getValue().equals(key));
     }
 }

src/org/openstreetmap/josm/gui/tagging/ac/AutoCompletionListItem.java
@@ -85,7 +85,7 @@ public class AutoCompletionListItem implements Comparable<AutoCompletionListItem
      * @throws IllegalArgumentException if value if null
      */
     public void setValue(String value) {
-        item.setValue(value);
+        throw new UnsupportedOperationException("setValue() is no longer supported");
     }
 
     @Override

src/org/openstreetmap/josm/gui/tagging/ac/AutoCompletionManager.java
@@ -331,7 +331,7 @@ public class AutoCompletionManager implements DataSetListener {
     }
 
     /**
-     * Populates the an {@link AutoCompletionList} with the currently cached values for some given tags
+     * Populates the {@link AutoCompletionList} with the currently cached values for some given tags
      *
      * @param list the list to populate
      * @param keys the tag keys