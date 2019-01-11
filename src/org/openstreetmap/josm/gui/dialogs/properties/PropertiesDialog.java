SamB/josm

commit 72931aa4cb6e3fadd202c409bad1821bef1de36c

see #17040 implement resetSelection() to forget recently selected primitives
git-svn-id: https://josm.openstreetmap.de/svn/trunk@14509 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/properties/PropertiesDialog.java
@@ -567,6 +567,8 @@ implements DataSelectionListener, ActiveLayerChangeListener, DataSetListenerAdap
     public void selectionChanged(SelectionChangeEvent event) {
         if (!isVisible())
             return;
+        if (event != null && event.getSelection().isEmpty())
+            editHelper.resetSelection();
         if (tagTable == null)
             return; // selection changed may be received in base class constructor before init
         if (tagTable.getCellEditor() != null) {
@@ -707,6 +709,7 @@ implements DataSelectionListener, ActiveLayerChangeListener, DataSetListenerAdap
     public void activeOrEditLayerChanged(ActiveLayerChangeEvent e) {
         if (e.getSource().getEditLayer() == null) {
             editHelper.saveTagsIfNeeded();
+            editHelper.resetSelection();
         }
         // it is time to save history of tags
         updateSelection();

src/org/openstreetmap/josm/gui/dialogs/properties/TagEditHelper.java
@@ -132,6 +132,7 @@ public class TagEditHelper {
     /** The preference storage of recent tags */
     public static final ListProperty PROPERTY_RECENT_TAGS = new ListProperty("properties.recent-tags",
             Collections.<String>emptyList());
+    /** The preference list of tags which should not be remembered, since r9940 */
     public static final StringProperty PROPERTY_TAGS_TO_IGNORE = new StringProperty("properties.recent-tags.ignore",
             new SearchSetting().writeToString());
 
@@ -364,6 +365,14 @@ public class TagEditHelper {
         if (PROPERTY_REMEMBER_TAGS.get() && !recentTags.isEmpty()) {
             recentTags.saveToPreference(PROPERTY_RECENT_TAGS);
         }
+    }
+
+    /**
+     * Forget recently selected primitives to allow GC.
+     * @since 14509
+     */
+    public void resetSelection() {
+        sel = null;
     }
 
     /**

commit 05027dd5d472a79a3089ffa210dbcb502b3e9870

fix #17197 (regression from r14509)
Only reset selection when last edit layer is closed

git-svn-id: https://josm.openstreetmap.de/svn/trunk@14673 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/properties/PropertiesDialog.java
@@ -567,8 +567,6 @@ implements DataSelectionListener, ActiveLayerChangeListener, DataSetListenerAdap
     public void selectionChanged(SelectionChangeEvent event) {
         if (!isVisible())
             return;
-        if (event != null && event.getSelection().isEmpty())
-            editHelper.resetSelection();
         if (tagTable == null)
             return; // selection changed may be received in base class constructor before init
         if (tagTable.getCellEditor() != null) {