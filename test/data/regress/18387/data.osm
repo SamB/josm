SamB/josm

commit 62425b5ac8a1d4808930960c9479ae18d5e20737

see #18368: Drastically improve performance of NodeGraph.buildSpanningPath() used in combine way 'C' and parallel ways map mode In special cases the old code was extremely slow, for example when you try to connect the ways of a complex multipolygon containing inner rings which always should fail. - detect unconnected graph - use extra HashSet for check if a NodePair is already in the path to avoid sequential search - avoid to test start points which cannot be the start point in a "spanning path"
git-svn-id: https://josm.openstreetmap.de/svn/trunk@15554 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/data/osm/NodeGraph.java
@@ -1,17 +1,24 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.data.osm;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Set;
 import java.util.Stack;
+import java.util.TreeMap;
 
 import org.openstreetmap.josm.tools.Pair;
 
@@ -135,26 +142,16 @@ public class NodeGraph {
     private final Map<Node, List<NodePair>> predecessors = new LinkedHashMap<>();
 
     protected void rememberSuccessor(NodePair pair) {
-        if (successors.containsKey(pair.getA())) {
-            if (!successors.get(pair.getA()).contains(pair)) {
-                successors.get(pair.getA()).add(pair);
-            }
-        } else {
-            List<NodePair> l = new ArrayList<>();
+        List<NodePair> l = successors.computeIfAbsent(pair.getA(), k -> new ArrayList<>());
+        if (!l.contains(pair)) {
             l.add(pair);
-            successors.put(pair.getA(), l);
         }
     }
 
     protected void rememberPredecessors(NodePair pair) {
-        if (predecessors.containsKey(pair.getB())) {
-            if (!predecessors.get(pair.getB()).contains(pair)) {
-                predecessors.get(pair.getB()).add(pair);
-            }
-        } else {
-            List<NodePair> l = new ArrayList<>();
+        List<NodePair> l = predecessors.computeIfAbsent(pair.getB(), k -> new ArrayList<>());
+        if (!l.contains(pair)) {
             l.add(pair);
-            predecessors.put(pair.getB(), l);
         }
     }
 
@@ -239,7 +236,7 @@ public class NodeGraph {
         return nodes;
     }
 
-    protected boolean isSpanningWay(Stack<NodePair> way) {
+    protected boolean isSpanningWay(Collection<NodePair> way) {
         return numUndirectedEges == way.size();
     }
 
@@ -262,16 +259,19 @@ public class NodeGraph {
      */
     protected List<Node> buildSpanningPath(Node startNode) {
         if (startNode != null) {
+            // do not simply replace `Stack` by `ArrayDeque` because of different iteration behaviour, see #11957
             Stack<NodePair> path = new Stack<>();
+            Set<NodePair> dupCheck = new HashSet<>();
             Stack<NodePair> nextPairs = new Stack<>();
             nextPairs.addAll(getOutboundPairs(startNode));
             while (!nextPairs.isEmpty()) {
                 NodePair cur = nextPairs.pop();
-                if (!path.contains(cur) && !path.contains(cur.swap())) {
+                if (!dupCheck.contains(cur) && !dupCheck.contains(cur.swap())) {
                     while (!path.isEmpty() && !path.peek().isPredecessorOf(cur)) {
-                        path.pop();
+                        dupCheck.remove(path.pop());
                     }
                     path.push(cur);
+                    dupCheck.add(cur);
                     if (isSpanningWay(path)) return buildPathFromNodePairs(path);
                     nextPairs.addAll(getOutboundPairs(path.peek()));
                 }
@@ -283,25 +283,84 @@ public class NodeGraph {
     /**
      * Tries to find a path through the graph which visits each edge (i.e.
      * the segment of a way) exactly once.
+     * <p><b>Note that duplicated edges are removed first!</b>
      *
      * @return the path; null, if no path was found
      */
     public List<Node> buildSpanningPath() {
         prepare();
-        // try to find a path from each "terminal node", i.e. from a
-        // node which is connected by exactly one undirected edges (or
-        // two directed edges in opposite direction) to the graph. A
-        // graph built up from way segments is likely to include such
-        // nodes, unless all ways are closed.
-        // In the worst case this loops over all nodes which is very slow for large ways.
-        //
-        Set<Node> nodes = getTerminalNodes();
-        nodes = nodes.isEmpty() ? getNodes() : nodes;
-        for (Node n: nodes) {
-            List<Node> path = buildSpanningPath(n);
-            if (!path.isEmpty())
-                return path;
+        if(numUndirectedEges > 0 && isConnected()) {
+            // try to find a path from each "terminal node", i.e. from a
+            // node which is connected by exactly one undirected edges (or
+            // two directed edges in opposite direction) to the graph. A
+            // graph built up from way segments is likely to include such
+            // nodes, unless the edges build one or more closed rings.
+            // We order the nodes to start with the best candidates, but
+            // it might take very long if there is no valid path as we iterate over all nodes
+            // to find out.
+            Set<Node> nodes = getTerminalNodes();
+            nodes = nodes.isEmpty() ? getMostFrequentVisitedNodesFirst() : nodes;
+            for (Node n : nodes) {
+                List<Node> path = buildSpanningPath(n);
+                if (!path.isEmpty())
+                    return path;
+            }
         }
         return null;
     }
+
+    /**
+     * Find out if the graph is connected.
+     * @return true if it is connected.
+     */
+    private boolean isConnected() {
+        Set<Node> nodes = getNodes();
+        if (nodes.isEmpty())
+            return false;
+        Deque<Node> toVisit = new ArrayDeque<>();
+        HashSet<Node> visited = new HashSet<>();
+        toVisit.add(nodes.iterator().next());
+        while(!toVisit.isEmpty()) {
+            Node n = toVisit.pop();
+            if (!visited.contains(n)) {
+                List<NodePair> neighbours = getOutboundPairs(n);
+                for (NodePair pair : neighbours) {
+                    toVisit.addLast(pair.getA());
+                    toVisit.addLast(pair.getB());
+                }
+                visited.add(n);
+            }
+        }
+        return nodes.size() == visited.size();
+    }
+
+    /**
+     * Sort the nodes by number of appearances in the edges.
+     * @return set of nodes which can be start nodes in a spanning way.
+     */
+    private Set<Node> getMostFrequentVisitedNodesFirst() {
+        if (edges.isEmpty())
+            return Collections.emptySet();
+        // count appearance of nodes in edges
+        Map<Node, Integer> counters = new HashMap<>();
+        for (NodePair pair : edges) {
+            Integer c = counters.get(pair.getA());
+            counters.put(pair.getA(), c == null ? 1 : c + 1);
+            c = counters.get(pair.getB());
+            counters.put(pair.getB(), c == null ? 1 : c + 1);
+        }
+        // group by counters
+        TreeMap<Integer, Set<Node>> sortedMap = new TreeMap<>(Comparator.reverseOrder());
+        for (Entry<Node, Integer> e : counters.entrySet()) {
+            sortedMap.computeIfAbsent(e.getValue(), LinkedHashSet::new).add(e.getKey());
+        }
+        LinkedHashSet<Node> result = new LinkedHashSet<>();
+        for (Entry<Integer, Set<Node>> e : sortedMap.entrySet()) {
+            if (e.getKey() > 4 || result.isEmpty()) {
+                result.addAll(e.getValue());
+            }
+        }
+        return result;
+    }
+
 }

commit 1274c2fef4bca18e517d2343f28ad09f37fd54a3

fix #18387 Combine way action refuses to combine new way with oneway (regression from r15554) I somehow expected that NodeGraph.getOutboundPairs(Node node) would return predessors and successors. Maybe that routine should be renamed to getSuccessorPairs?
git-svn-id: https://josm.openstreetmap.de/svn/trunk@15558 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/data/osm/NodeGraph.java
@@ -220,6 +220,13 @@ public class NodeGraph {
         return ret;
     }
 
+    private List<NodePair> getConnectedPairs (Node node) {
+        List<NodePair> connected = new ArrayList<>();
+        connected.addAll(Optional.ofNullable(successors.get(node)).orElseGet(Collections::emptyList));
+        connected.addAll(Optional.ofNullable(predecessors.get(node)).orElseGet(Collections::emptyList));
+        return connected;
+    }
+
     protected List<NodePair> getOutboundPairs(NodePair pair) {
         return getOutboundPairs(pair.getB());
     }
@@ -339,10 +346,11 @@ public class NodeGraph {
         while (!toVisit.isEmpty()) {
             Node n = toVisit.pop();
             if (!visited.contains(n)) {
-                List<NodePair> neighbours = getOutboundPairs(n);
-                for (NodePair pair : neighbours) {
-                    toVisit.addLast(pair.getA());
-                    toVisit.addLast(pair.getB());
+                for (NodePair pair : getConnectedPairs(n)) {
+                    if (n != pair.getA())
+                        toVisit.addLast(pair.getA());
+                    if (n != pair.getB())
+                        toVisit.addLast(pair.getB());
                 }
                 visited.add(n);
             }

test/data/regress/18387/data.osm
@@ -1,0 +1,38 @@
+<?xml version='1.0' encoding='UTF-8'?>
+<osm version='0.6' generator='JOSM'>
+  <node id='-180907' action='modify' visible='true' lat='52.29588639682' lon='13.64411897386' />
+  <node id='-180909' action='modify' visible='true' lat='52.29609561925' lon='13.64572029375' />
+  <node id='61146250' timestamp='2014-11-05T19:19:33Z' uid='115651' user='Rego_Loos' visible='true' version='7' changeset='26578210' lat='52.2948763' lon='13.6442384' />
+  <node id='263019068' timestamp='2014-05-08T09:07:16Z' uid='115651' user='Rego_Loos' visible='true' version='5' changeset='22204763' lat='52.2953129' lon='13.6466991' />
+  <node id='263019078' timestamp='2014-11-05T19:19:34Z' uid='115651' user='Rego_Loos' visible='true' version='6' changeset='26578210' lat='52.2949636' lon='13.6434815' />
+  <node id='263019090' timestamp='2014-05-08T09:07:16Z' uid='115651' user='Rego_Loos' visible='true' version='4' changeset='22204763' lat='52.2952933' lon='13.641262' />
+  <node id='264010827' timestamp='2014-11-05T19:19:34Z' uid='115651' user='Rego_Loos' visible='true' version='5' changeset='26578210' lat='52.2948715' lon='13.6450454' />
+  <node id='264010828' timestamp='2014-11-05T19:19:34Z' uid='115651' user='Rego_Loos' visible='true' version='5' changeset='26578210' lat='52.2949126' lon='13.6453051' />
+  <node id='293894042' timestamp='2014-11-05T19:19:35Z' uid='115651' user='Rego_Loos' visible='true' version='3' changeset='26578210' lat='52.2949155' lon='13.6438623' />
+  <node id='428651766' timestamp='2014-05-08T09:07:18Z' uid='115651' user='Rego_Loos' visible='true' version='2' changeset='22204763' lat='52.2948714' lon='13.6447485' />
+  <node id='2153001668' timestamp='2014-05-08T09:07:17Z' uid='115651' user='Rego_Loos' visible='true' version='2' changeset='22204763' lat='52.295224' lon='13.6416799' />
+  <node id='2153001679' timestamp='2014-11-05T19:19:35Z' uid='115651' user='Rego_Loos' visible='true' version='3' changeset='26578210' lat='52.2949787' lon='13.6455675' />
+  <way id='-180920' action='modify' visible='true'>
+    <nd ref='-180907' />
+    <nd ref='-180909' />
+    <nd ref='263019068' />
+  </way>
+  <way id='24290495' timestamp='2018-05-28T12:52:28Z' uid='2680179' user='SGKW' visible='true' version='11' changeset='59339679'>
+    <nd ref='263019090' />
+    <nd ref='2153001668' />
+    <nd ref='263019078' />
+    <nd ref='293894042' />
+    <nd ref='61146250' />
+    <nd ref='428651766' />
+    <nd ref='264010827' />
+    <nd ref='264010828' />
+    <nd ref='2153001679' />
+    <nd ref='263019068' />
+    <tag k='cycleway' v='opposite' />
+    <tag k='highway' v='residential' />
+    <tag k='maxspeed' v='30' />
+    <tag k='name' v='Kirchsteig' />
+    <tag k='oneway' v='yes' />
+    <tag k='surface' v='asphalt' />
+  </way>
+</osm>

test/unit/org/openstreetmap/josm/actions/CombineWayActionTest.java
@@ -3,9 +3,12 @@ package org.openstreetmap.josm.actions;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -17,6 +20,7 @@ import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.NodeGraph;
 import org.openstreetmap.josm.data.osm.NodePair;
+import org.openstreetmap.josm.data.osm.Way;
 import org.openstreetmap.josm.io.IllegalDataException;
 import org.openstreetmap.josm.io.OsmReader;
 import org.openstreetmap.josm.testutils.JOSMTestRules;
@@ -59,7 +63,7 @@ public class CombineWayActionTest {
     }
 
     /**
-     * Non-regression test for bug #1835 (combine way with overlapping ways)
+     * Non-regression test for bug #18385 (combine way with overlapping ways)
      * @throws IOException if any I/O error occurs
      * @throws IllegalDataException if OSM parsing fails
      */
@@ -70,6 +74,25 @@ public class CombineWayActionTest {
             NodeGraph graph = NodeGraph.createNearlyUndirectedGraphFromNodeWays(ds.getWays());
             List<Node> path = graph.buildSpanningPathNoRemove();
             assertNull(path);
+        }
+    }
+
+    /**
+     * Non-regression test for bug #18387 (combine new way with oneway)
+     * @throws IOException if any I/O error occurs
+     * @throws IllegalDataException if OSM parsing fails
+     */
+    @Test
+    public void testTicket18387() throws IOException, IllegalDataException {
+        try (InputStream is = TestUtils.getRegressionDataStream(18387, "data.osm")) {
+            DataSet ds = OsmReader.parseDataSet(is, null);
+            ArrayList<Way> selection = new ArrayList<>(ds.getWays());
+            assertEquals(2, selection.size());
+            if (!selection.get(0).isNew())
+                Collections.reverse(selection);
+            NodeGraph graph = NodeGraph.createNearlyUndirectedGraphFromNodeWays(selection);
+            List<Node> path = graph.buildSpanningPathNoRemove();
+            assertTrue(path != null);
         }
     }
 