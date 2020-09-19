SamB/josm

commit 7de908c382327f24df9ac7e327869a687b40bf40

fix #18679 - Autofilter: extract numeric value from number+unit
git-svn-id: https://josm.openstreetmap.de/svn/trunk@15838 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/autofilter/AutoFilterManager.java
@@ -7,20 +7,17 @@ import java.awt.Graphics2D;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Comparator;
-import java.util.Iterator;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.NavigableSet;
 import java.util.Objects;
-import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import org.openstreetmap.josm.actions.mapmode.MapMode;
 import org.openstreetmap.josm.data.osm.BBox;
@@ -93,7 +90,7 @@ implements ZoomChangeListener, MapModeChangeListener, DataSetListener, Preferenc
     /**
      * The buttons currently displayed in map view.
      */
-    private final Map<String, AutoFilterButton> buttons = new TreeMap<>();
+    private final Map<Integer, AutoFilterButton> buttons = new TreeMap<>();
 
     /**
      * The list of registered auto filter rules.
@@ -145,7 +142,7 @@ implements ZoomChangeListener, MapModeChangeListener, DataSetListener, Preferenc
         if (enabledRule != null && map != null
                 && enabledRule.getMinZoomLevel() <= Selector.GeneralSelector.scale2level(map.mapView.getDist100Pixel())) {
             // Retrieve the values from current rule visible on screen
-            NavigableSet<String> values = getNumericValues(enabledRule.getKey(), enabledRule.getValueComparator());
+            NavigableSet<Integer> values = getNumericValues(enabledRule.getKey());
             // Make sure current auto filter button remains visible even if no data is found, to allow user to disable it
             if (currentAutoFilter != null) {
                 values.add(currentAutoFilter.getFilter().value);
@@ -157,11 +154,11 @@ implements ZoomChangeListener, MapModeChangeListener, DataSetListener, Preferenc
         }
     }
 
-    static class CompiledFilter extends Filter implements MatchSupplier {
+    class CompiledFilter extends Filter implements MatchSupplier {
         final String key;
-        final String value;
+        final int value;
 
-        CompiledFilter(String key, String value) {
+        CompiledFilter(String key, int value) {
             this.key = key;
             this.value = value;
             this.enable = true;
@@ -174,21 +171,21 @@ implements ZoomChangeListener, MapModeChangeListener, DataSetListener, Preferenc
             return new SearchCompiler.Match() {
                 @Override
                 public boolean match(OsmPrimitive osm) {
-                    return getTagValuesForPrimitive(key, osm).anyMatch(value::equals);
+                    return getTagValuesForPrimitive(key, osm).anyMatch(v -> v == value);
                 }
             };
         }
     }
 
-    private synchronized void addNewButtons(NavigableSet<String> values) {
+    private synchronized void addNewButtons(NavigableSet<Integer> values) {
         if (values.isEmpty()) {
             return;
         }
         int i = 0;
         int maxWidth = 16;
         final AutoFilterButton keyButton = AutoFilterButton.forOsmKey(enabledRule.getKey());
-        addButton(keyButton, Integer.toString(Integer.MIN_VALUE), i++);
-        for (final String value : values.descendingSet()) {
+        addButton(keyButton, Integer.MIN_VALUE, i++);
+        for (final Integer value : values.descendingSet()) {
             CompiledFilter filter = new CompiledFilter(enabledRule.getKey(), value);
             String label = enabledRule.getValueFormatter().apply(value);
             AutoFilter autoFilter = new AutoFilter(label, filter.text, filter);
@@ -205,71 +202,55 @@ implements ZoomChangeListener, MapModeChangeListener, DataSetListener, Preferenc
         MainApplication.getMap().mapView.validate();
     }
 
-    private void addButton(AutoFilterButton button, String value, int i) {
+    private void addButton(AutoFilterButton button, int value, int i) {
         MapView mapView = MainApplication.getMap().mapView;
         buttons.put(value, button);
         mapView.add(button).setLocation(3, 60 + 22*i);
     }
 
     private void removeAllButtons() {
-        for (Iterator<String> it = buttons.keySet().iterator(); it.hasNext();) {
-            MainApplication.getMap().mapView.remove(buttons.get(it.next()));
-            it.remove();
-        }
+        buttons.values().forEach(MainApplication.getMap().mapView::remove);
+        buttons.clear();
     }
 
-    private static NavigableSet<String> getNumericValues(String key, Comparator<String> comparator) {
-        NavigableSet<String> values = new TreeSet<>(comparator);
-        for (String s : getTagValues(key)) {
-            try {
-                Integer.parseInt(s);
-                values.add(s);
-            } catch (NumberFormatException e) {
-                Logging.trace(e);
-            }
-        }
-        return values;
-    }
-
-    private static Set<String> getTagValues(String key) {
+    private NavigableSet<Integer> getNumericValues(String key) {
         DataSet ds = MainApplication.getLayerManager().getActiveDataSet();
-        Set<String> values = new TreeSet<>();
-        if (ds != null) {
-            BBox bbox = MainApplication.getMap().mapView.getState().getViewArea().getLatLonBoundsBox().toBBox();
-            Consumer<OsmPrimitive> consumer = o -> getTagValuesForPrimitive(key, o).forEach(values::add);
-            ds.searchNodes(bbox).forEach(consumer);
-            ds.searchWays(bbox).forEach(consumer);
-            ds.searchRelations(bbox).forEach(consumer);
+        if (ds == null) {
+            return Collections.emptyNavigableSet();
         }
+        BBox bbox = MainApplication.getMap().mapView.getState().getViewArea().getLatLonBoundsBox().toBBox();
+        NavigableSet<Integer> values = new TreeSet<>();
+        Consumer<OsmPrimitive> consumer = o -> getTagValuesForPrimitive(key, o).forEach(values::add);
+        ds.searchNodes(bbox).forEach(consumer);
+        ds.searchWays(bbox).forEach(consumer);
+        ds.searchRelations(bbox).forEach(consumer);
         return values;
     }
 
-    static Stream<String> getTagValuesForPrimitive(String key, OsmPrimitive osm) {
+    protected IntStream getTagValuesForPrimitive(String key, OsmPrimitive osm) {
+        if (enabledRule == null) {
+            return IntStream.empty();
+        }
         String value = osm.get(key);
         if (value != null) {
             Pattern p = Pattern.compile("(-?[0-9]+)-(-?[0-9]+)");
-            return OsmUtils.splitMultipleValues(value).flatMap(v -> {
+            return OsmUtils.splitMultipleValues(value).flatMapToInt(v -> {
                 Matcher m = p.matcher(v);
                 if (m.matches()) {
                     int a = Integer.parseInt(m.group(1));
                     int b = Integer.parseInt(m.group(2));
-                    return IntStream.rangeClosed(Math.min(a, b), Math.max(a, b))
-                            .mapToObj(Integer::toString);
+                    return IntStream.rangeClosed(Math.min(a, b), Math.max(a, b));
                 } else {
-                    return Stream.of(v);
+                    try {
+                        return IntStream.of(enabledRule.getValueExtractor().applyAsInt(v));
+                    } catch (NumberFormatException e) {
+                        Logging.trace(e);
+                        return IntStream.empty();
+                    }
                 }
             });
-        } else if (PROP_AUTO_FILTER_DEFAULTS.get() && "layer".equals(key)) {
-            // assume sensible defaults, see #17496
-            if (osm.hasTag("bridge") || osm.hasTag("power", "line") || osm.hasTag("location", "overhead")) {
-                return Stream.of("1");
-            } else if (osm.isKeyTrue("tunnel") || osm.hasTag("tunnel", "culvert") || osm.hasTag("location", "underground")) {
-                return Stream.of("-1");
-            } else if (osm.hasTag("tunnel", "building_passage") || osm.hasKey("highway", "railway", "waterway")) {
-                return Stream.of("0");
-            }
         }
-        return Stream.empty();
+        return enabledRule.getDefaultValueSupplier().apply(osm);
     }
 
     @Override

src/org/openstreetmap/josm/gui/autofilter/AutoFilterRule.java
@@ -1,10 +1,15 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.gui.autofilter;
 
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.function.Function;
-import java.util.function.UnaryOperator;
+import java.util.function.IntFunction;
+import java.util.function.ToIntFunction;
+import java.util.stream.IntStream;
+
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
 
 /**
  * An auto filter rule determines how auto filter can be built from visible map data.
@@ -19,9 +24,11 @@ public class AutoFilterRule {
 
     private final int minZoomLevel;
 
-    private UnaryOperator<String> valueFormatter = s -> s;
+    private Function<OsmPrimitive, IntStream> defaultValueSupplier = p -> IntStream.empty();
 
-    private Comparator<String> valueComparator = Comparator.comparingInt(s -> Integer.parseInt(valueFormatter.apply(s)));
+    private ToIntFunction<String> valueExtractor = Integer::parseInt;
+
+    private IntFunction<String> valueFormatter = Integer::toString;
 
     /**
      * Constructs a new {@code AutoFilterRule}.
@@ -53,7 +60,7 @@ public class AutoFilterRule {
      * Returns the OSM value formatter that defines the associated button label.
      * @return the OSM value formatter that defines the associated button label (identity by default)
      */
-    public Function<String, String> getValueFormatter() {
+    public IntFunction<String> getValueFormatter() {
         return valueFormatter;
     }
 
@@ -63,27 +70,47 @@ public class AutoFilterRule {
      * @return {@code this}
      * @throws NullPointerException if {@code valueFormatter} is null
      */
-    public AutoFilterRule setValueFormatter(UnaryOperator<String> valueFormatter) {
+    public AutoFilterRule setValueFormatter(IntFunction<String> valueFormatter) {
         this.valueFormatter = Objects.requireNonNull(valueFormatter);
         return this;
     }
 
     /**
-     * Returns the OSM value comparator used to order the buttons.
-     * @return the OSM value comparator
+     * Returns a function which yields default values for the given OSM primitive
+     * @return a function which yields default values for the given OSM primitive
      */
-    public Comparator<String> getValueComparator() {
-        return valueComparator;
+    public Function<OsmPrimitive, IntStream> getDefaultValueSupplier() {
+        return defaultValueSupplier;
     }
 
     /**
-     * Sets the OSM value comparator used to order the buttons.
-     * @param valueComparator the OSM value comparator
+     * Sets the function which yields default values for the given OSM primitive.
+     * This function is invoked if the primitive does not have this {@linkplain #getKey() key}.
+     * @param defaultValueSupplier the function which yields default values for the given OSM primitive
      * @return {@code this}
-     * @throws NullPointerException if {@code valueComparator} is null
+     * @throws NullPointerException if {@code defaultValueSupplier} is null
      */
-    public AutoFilterRule setValueComparator(Comparator<String> valueComparator) {
-        this.valueComparator = valueComparator;
+    public AutoFilterRule setDefaultValueSupplier(Function<OsmPrimitive, IntStream> defaultValueSupplier) {
+        this.defaultValueSupplier = Objects.requireNonNull(defaultValueSupplier);
+        return this;
+    }
+
+    /**
+     * Returns a function which extracts a numeric value from an OSM value
+     * @return a function which extracts a numeric value from an OSM value
+     */
+    public ToIntFunction<String> getValueExtractor() {
+        return valueExtractor;
+    }
+
+    /**
+     * Sets the function which extracts a numeric value from an OSM value
+     * @param valueExtractor the function which extracts a numeric value from an OSM value
+     * @return {@code this}
+     * @throws NullPointerException if {@code valueExtractor} is null
+     */
+    public AutoFilterRule setValueExtractor(ToIntFunction<String> valueExtractor) {
+        this.valueExtractor = Objects.requireNonNull(valueExtractor);
         return this;
     }
 
@@ -92,15 +119,47 @@ public class AutoFilterRule {
      * @return the default list of auto filter rules
      */
     public static AutoFilterRule[] defaultRules() {
-        return new AutoFilterRule[] {
+        return new AutoFilterRule[]{
             new AutoFilterRule("level", 17),
-            new AutoFilterRule("layer", 16),
+            new AutoFilterRule("layer", 16)
+                    .setDefaultValueSupplier(AutoFilterRule::defaultLayer),
             new AutoFilterRule("maxspeed", 16)
-                .setValueFormatter(s -> s.replace(" mph", "")),
+                    .setValueExtractor(s -> Integer.parseInt(s.replace(" mph", ""))),
             new AutoFilterRule("voltage", 5)
-                .setValueFormatter(s -> s.replaceAll("000$", "k") + 'V')
-                .setValueComparator(Comparator.comparingInt(Integer::parseInt))
+                    .setValueFormatter(s -> s % 1000 == 0 ? (s / 1000) + "kV" : s + "V"),
+            new AutoFilterRule("building:levels", 17),
+            new AutoFilterRule("gauge", 5),
+            new AutoFilterRule("frequency", 5),
+            new AutoFilterRule("incline", 13)
+                    .setValueExtractor(s -> Integer.parseInt(s.replaceAll("%$", "")))
+                    .setValueFormatter(v -> v + "\u2009%"),
+            new AutoFilterRule("lanes", 13),
+            new AutoFilterRule("admin_level", 11)
         };
+    }
+
+    /**
+     * Returns the default auto filter rule for the given key
+     * @param key the OSM key
+     * @return default auto filter rule for the given key
+     */
+    static Optional<AutoFilterRule> getDefaultRule(String key) {
+        return Arrays.stream(AutoFilterRule.defaultRules())
+                .filter(r -> key.equals(r.getKey()))
+                .findFirst();
+    }
+
+    private static IntStream defaultLayer(OsmPrimitive osm) {
+        // assume sensible defaults, see #17496
+        if (osm.hasTag("bridge") || osm.hasTag("power", "line") || osm.hasTag("location", "overhead")) {
+            return IntStream.of(1);
+        } else if (osm.isKeyTrue("tunnel") || osm.hasTag("tunnel", "culvert") || osm.hasTag("location", "underground")) {
+            return IntStream.of(-1);
+        } else if (osm.hasTag("tunnel", "building_passage") || osm.hasKey("highway", "railway", "waterway")) {
+            return IntStream.of(0);
+        } else {
+            return IntStream.empty();
+        }
     }
 
     @Override

test/unit/org/openstreetmap/josm/gui/autofilter/AutoFilterManagerTest.java
@@ -2,9 +2,9 @@
 package org.openstreetmap.josm.gui.autofilter;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
 import java.util.Arrays;
+import java.util.OptionalInt;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -33,7 +33,8 @@ public class AutoFilterManagerTest {
      */
     @Test
     public void testTagValuesForPrimitive() {
-        final TreeSet<String> values = Stream.of(
+        AutoFilterManager.getInstance().setCurrentAutoFilter(null);
+        final TreeSet<Integer> values = Stream.of(
                 OsmUtils.createPrimitive("way level=-4--5"),
                 OsmUtils.createPrimitive("way level=-2"),
                 OsmUtils.createPrimitive("node level=0"),
@@ -41,9 +42,10 @@ public class AutoFilterManagerTest {
                 OsmUtils.createPrimitive("way level=2;3"),
                 OsmUtils.createPrimitive("way level=6-9"),
                 OsmUtils.createPrimitive("way level=10;12-13"))
-                .flatMap(o -> AutoFilterManager.getTagValuesForPrimitive("level", o))
+                .flatMapToInt(o -> AutoFilterManager.getInstance().getTagValuesForPrimitive("level", o))
+                .boxed()
                 .collect(Collectors.toCollection(TreeSet::new));
-        assertEquals(new TreeSet<>(Arrays.asList("-5", "-4", "-2", "0", "1", "2", "3", "6", "7", "8", "9", "10", "12", "13")), values);
+        assertEquals(new TreeSet<>(Arrays.asList(-5, -4, -2, 0, 1, 2, 3, 6, 7, 8, 9, 10, 12, 13)), values);
 
     }
 
@@ -52,19 +54,22 @@ public class AutoFilterManagerTest {
      */
     @Test
     public void testTagValuesForPrimitivesDefaults() {
-        assertNull(getLayer("way foo=bar"));
-        assertEquals("1", getLayer("way bridge=yes"));
-        assertEquals("1", getLayer("way power=line"));
-        assertEquals("-1", getLayer("way tunnel=yes"));
-        assertEquals("0", getLayer("way tunnel=building_passage"));
-        assertEquals("0", getLayer("way highway=residential"));
-        assertEquals("0", getLayer("way railway=rail"));
-        assertEquals("0", getLayer("way waterway=canal"));
+        AutoFilterManager.getInstance().setCurrentAutoFilter(null);
+        assertEquals(OptionalInt.empty(), getLayer("way foo=bar"));
+        AutoFilterRule.getDefaultRule("layer").ifPresent(AutoFilterManager.getInstance()::enableAutoFilterRule);
+        assertEquals(OptionalInt.empty(), getLayer("way foo=bar"));
+        assertEquals(OptionalInt.of(1), getLayer("way bridge=yes"));
+        assertEquals(OptionalInt.of(1), getLayer("way power=line"));
+        assertEquals(OptionalInt.of(-1), getLayer("way tunnel=yes"));
+        assertEquals(OptionalInt.of(0), getLayer("way tunnel=building_passage"));
+        assertEquals(OptionalInt.of(0), getLayer("way highway=residential"));
+        assertEquals(OptionalInt.of(0), getLayer("way railway=rail"));
+        assertEquals(OptionalInt.of(0), getLayer("way waterway=canal"));
     }
 
-    private String getLayer(final String assertion) {
-        return AutoFilterManager.getTagValuesForPrimitive("layer", OsmUtils.createPrimitive(assertion))
-                .findFirst()
-                .orElse(null);
+    private OptionalInt getLayer(final String assertion) {
+        return AutoFilterManager.getInstance()
+                .getTagValuesForPrimitive("layer", OsmUtils.createPrimitive(assertion))
+                .findFirst();
     }
 }

commit 00ee50344cafccb8dbe78b593ccc18fc74dcb44d

fix #19817: NPE when changing autofilter option while no layer is visible Add null check (regression from r15838)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@17039 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/autofilter/AutoFilterManager.java
@@ -223,7 +223,10 @@ implements ZoomChangeListener, MapModeChangeListener, DataSetListener, Preferenc
     }
 
     private void removeAllButtons() {
-        buttons.values().forEach(MainApplication.getMap().mapView::remove);
+        MapFrame map= MainApplication.getMap();
+        if (map != null) {
+            buttons.values().forEach(map.mapView::remove);
+        }
         buttons.clear();
     }
 