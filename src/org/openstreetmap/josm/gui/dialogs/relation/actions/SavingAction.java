SamB/josm

commit f3751e8cd5e35ccaecde6ab22517e9f30afa0fd6

see #19885: memory leak with "temporary" objects in validator and actions - (hopefully) fix memory leaks in complex actions - handle complex cases with presets and `RelationEditor`
I hope these changes don't break plugins which extend or overwrite `RelationEditor`

git-svn-id: https://josm.openstreetmap.de/svn/trunk@17358 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/relation/actions/SavingAction.java
@@ -4,12 +4,17 @@ package org.openstreetmap.josm.gui.dialogs.relation.actions;
 import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.Component;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
 import org.openstreetmap.josm.command.AddCommand;
 import org.openstreetmap.josm.command.ChangeCommand;
+import org.openstreetmap.josm.command.ChangeMembersCommand;
+import org.openstreetmap.josm.command.ChangePropertyCommand;
+import org.openstreetmap.josm.command.Command;
 import org.openstreetmap.josm.command.conflict.ConflictAddCommand;
 import org.openstreetmap.josm.data.UndoRedoHandler;
 import org.openstreetmap.josm.data.conflict.Conflict;
@@ -85,9 +90,17 @@ abstract class SavingAction extends AbstractRelationEditorAction {
         Relation editedRelation = new Relation(originRelation);
         tagEditorModel.applyToPrimitive(editedRelation);
         getMemberTableModel().applyToRelation(editedRelation);
-        if (!editedRelation.hasEqualSemanticAttributes(originRelation, false)) {
+        List<Command> cmds = new ArrayList<>();
+        if (originRelation.getKeys().equals(editedRelation.getKeys())) {
+            cmds.add(new ChangeMembersCommand(originRelation, editedRelation.getMembers()));
+        }
+        Command cmdProps = ChangePropertyCommand.build(originRelation, editedRelation);
+        if (cmdProps != null)
+            cmds.add(cmdProps);
+        if (cmds.size() >= 2) {
             UndoRedoHandler.getInstance().add(new ChangeCommand(originRelation, editedRelation));
-        } else {
+        } else if (!cmds.isEmpty()) {
+            UndoRedoHandler.getInstance().add(cmds.get(0));
             editedRelation.setMembers(null); // see #19885
         }
     }

src/org/openstreetmap/josm/actions/CreateMultipolygonAction.java
@@ -25,7 +25,7 @@ import javax.swing.SwingUtilities;
 
 import org.openstreetmap.josm.actions.relation.DownloadSelectedIncompleteMembersAction;
 import org.openstreetmap.josm.command.AddCommand;
-import org.openstreetmap.josm.command.ChangeCommand;
+import org.openstreetmap.josm.command.ChangeMembersCommand;
 import org.openstreetmap.josm.command.ChangePropertyCommand;
 import org.openstreetmap.josm.command.Command;
 import org.openstreetmap.josm.command.SequenceCommand;
@@ -115,6 +115,10 @@ public class CreateMultipolygonAction extends JosmAction {
 
             // to avoid EDT violations
             SwingUtilities.invokeLater(() -> {
+                if (multipolygonRelation != null) {
+                    // rather ugly: update generated a ChangeMembersCommand with a copy of the member list, so clear the list now
+                    commandAndRelation.b.setMembers(null); // #see 19885
+                }
                 UndoRedoHandler.getInstance().add(command);
                 final Relation relation = (Relation) MainApplication.getLayerManager().getEditDataSet()
                         .getPrimitiveById(commandAndRelation.b);
@@ -228,7 +232,8 @@ public class CreateMultipolygonAction extends JosmAction {
      * Returns a {@link Pair} of the old multipolygon {@link Relation} (or null) and the newly created/modified multipolygon {@link Relation}.
      * @param selectedWays selected ways
      * @param selectedMultipolygonRelation selected multipolygon relation
-     * @return pair of old and new multipolygon relation if a difference was found, else the pair contains the old relation twice
+     * @return null if ways don't build a valid multipolygon, pair of old and new multipolygon relation if a difference was found,
+     * else the pair contains the old relation twice
      */
     public static Pair<Relation, Relation> updateMultipolygonRelation(Collection<Way> selectedWays, Relation selectedMultipolygonRelation) {
 
@@ -243,6 +248,7 @@ public class CreateMultipolygonAction extends JosmAction {
             return mergeRelationsMembers(selectedMultipolygonRelation, calculated);
         }
         showErrors(mpTest.getErrors());
+        calculated.setMembers(null); // see #19885
         return null; //could not make multipolygon.
     }
 
@@ -279,6 +285,7 @@ public class CreateMultipolygonAction extends JosmAction {
             GuiHelper.runInEDT(() -> new Notification(msg).setIcon(JOptionPane.WARNING_MESSAGE).show());
         }
         foundDiff |= merged.addAll(calculated.getMembers());
+        calculated.setMembers(null); // see #19885
         if (!foundDiff) {
             return Pair.create(old, old); // unchanged
         }
@@ -342,7 +349,7 @@ public class CreateMultipolygonAction extends JosmAction {
             commandName = getName(false);
         } else {
             if (!unchanged) {
-                list.add(new ChangeCommand(existingRelation, relation));
+                list.add(new ChangeMembersCommand(existingRelation, new ArrayList<>(relation.getMembers())));
             }
             if (list.isEmpty()) {
                 if (unchanged) {
@@ -389,9 +396,10 @@ public class CreateMultipolygonAction extends JosmAction {
     private static final List<String> DEFAULT_LINEAR_TAGS = Arrays.asList("barrier", "fence_type", "source");
 
     /**
-     * This method removes tags/value pairs from inner and outer ways and put them on relation if necessary
+     * This method removes tags/value pairs from inner and outer ways and put them on relation if necessary.
      * Function was extended in reltoolbox plugin by Zverikk and copied back to the core
-     * @param relation the multipolygon style relation to process
+     * @param relation the multipolygon style relation to process. If it is new, the tags might be
+     * modified, else the list of commands will contain a command to modify its tags
      * @return a list of commands to execute
      */
     public static List<Command> removeTagsFromWaysIfNeeded(Relation relation) {
@@ -471,17 +479,18 @@ public class CreateMultipolygonAction extends JosmAction {
             }
         }
 
-        if (moveTags) {
+        values.remove("area");
+        if (moveTags && !values.isEmpty()) {
             // add those tag values to the relation
             boolean fixed = false;
-            Relation r2 = new Relation(relation);
+            Map<String, String> tagsToAdd = new HashMap<>();
             for (Entry<String, String> entry : values.entrySet()) {
                 String key = entry.getKey();
-                if (!r2.hasKey(key) && !"area".equals(key)) {
+                if (!relation.hasKey(key)) {
                     if (relation.isNew())
                         relation.put(key, entry.getValue());
                     else
-                        r2.put(key, entry.getValue());
+                        tagsToAdd.put(key, entry.getValue());
                     fixed = true;
                 }
             }
@@ -490,9 +499,7 @@ public class CreateMultipolygonAction extends JosmAction {
                 if (ds == null) {
                     ds = MainApplication.getLayerManager().getEditDataSet();
                 }
-                commands.add(new ChangeCommand(ds, relation, r2));
-            } else {
-                r2.setMembers(null); // see #19885
+                commands.add(new ChangePropertyCommand(ds, Collections.singleton(relation), tagsToAdd));
             }
         }
 

src/org/openstreetmap/josm/actions/JoinAreasAction.java
@@ -25,8 +25,9 @@ import javax.swing.JOptionPane;
 
 import org.openstreetmap.josm.actions.ReverseWayAction.ReverseWayResult;
 import org.openstreetmap.josm.command.AddCommand;
-import org.openstreetmap.josm.command.ChangeCommand;
+import org.openstreetmap.josm.command.ChangeMembersCommand;
 import org.openstreetmap.josm.command.ChangeNodesCommand;
+import org.openstreetmap.josm.command.ChangePropertyCommand;
 import org.openstreetmap.josm.command.Command;
 import org.openstreetmap.josm.command.DeleteCommand;
 import org.openstreetmap.josm.command.SequenceCommand;
@@ -1633,12 +1634,10 @@ public class JoinAreasAction extends JosmAction {
                     continue;
                 }
 
-                Relation newRel = new Relation(r);
-                List<RelationMember> members = newRel.getMembers();
+                List<RelationMember> members = r.getMembers();
                 members.remove(rm);
-                newRel.setMembers(members);
 
-                cmds.add(new ChangeCommand(r, newRel));
+                cmds.add(new ChangeMembersCommand(r, members));
                 RelationRole saverel = new RelationRole(r, rm.getRole());
                 if (!result.contains(saverel)) {
                     result.add(saverel);
@@ -1673,26 +1672,24 @@ public class JoinAreasAction extends JosmAction {
                 continue;
             }
             // Add it back!
-            Relation newRel = new Relation(r.rel);
-            newRel.addMember(new RelationMember(r.role, outer));
-            cmds.add(new ChangeCommand(r.rel, newRel));
+            List<RelationMember> modifiedMembers = new ArrayList<>(r.rel.getMembers());
+            modifiedMembers.add(new RelationMember(r.role, outer));
+            cmds.add(new ChangeMembersCommand(r.rel, modifiedMembers));
         }
 
-        Relation newRel;
-        RelationRole soleOuter;
         switch (multiouters.size()) {
         case 0:
             return;
         case 1:
             // Found only one to be part of a multipolygon relation, so just add it back as well
-            soleOuter = multiouters.get(0);
-            newRel = new Relation(soleOuter.rel);
-            newRel.addMember(new RelationMember(soleOuter.role, outer));
-            cmds.add(new ChangeCommand(ds, soleOuter.rel, newRel));
+            RelationRole soleOuter = multiouters.get(0);
+            List<RelationMember> modifiedMembers = new ArrayList<>(soleOuter.rel.getMembers());
+            modifiedMembers.add(new RelationMember(soleOuter.role, outer));
+            cmds.add(new ChangeMembersCommand(ds, soleOuter.rel, modifiedMembers));
             return;
         default:
             // Create a new relation with all previous members and (Way)outer as outer.
-            newRel = new Relation();
+            Relation newRel = new Relation();
             for (RelationRole r : multiouters) {
                 // Add members
                 for (RelationMember rm : r.rel.getMembers()) {
@@ -1717,11 +1714,11 @@ public class JoinAreasAction extends JosmAction {
      * @param ways The List of Ways to remove all tags from
      */
     private void stripTags(Collection<Way> ways) {
-        for (Way w : ways) {
-            final Way wayWithoutTags = new Way(w);
-            wayWithoutTags.removeAll();
-            cmds.add(new ChangeCommand(w, wayWithoutTags));
-        }
+        Map<String, String> tagsToRemove = new HashMap<>();
+        ways.stream().flatMap(w -> w.keySet().stream()).forEach(k -> tagsToRemove.put(k, null));
+        if (tagsToRemove.isEmpty())
+            return;
+        cmds.add(new ChangePropertyCommand(new ArrayList<>(ways), tagsToRemove));
         /* I18N: current action printed in status display */
         commitCommands(marktr("Remove tags from inner ways"));
     }

src/org/openstreetmap/josm/actions/ReverseWayAction.java
@@ -17,7 +17,7 @@ import javax.swing.JOptionPane;
 
 import org.openstreetmap.josm.actions.corrector.ReverseWayNoTagCorrector;
 import org.openstreetmap.josm.actions.corrector.ReverseWayTagCorrector;
-import org.openstreetmap.josm.command.ChangeCommand;
+import org.openstreetmap.josm.command.ChangeNodesCommand;
 import org.openstreetmap.josm.command.Command;
 import org.openstreetmap.josm.command.SequenceCommand;
 import org.openstreetmap.josm.data.UndoRedoHandler;
@@ -40,28 +40,17 @@ public final class ReverseWayAction extends JosmAction {
      * The resulting way after reversing it and the commands to get there.
      */
     public static class ReverseWayResult {
-        private final Way newWay;
         private final Collection<Command> tagCorrectionCommands;
         private final Command reverseCommand;
 
         /**
          * Create a new {@link ReverseWayResult}
-         * @param newWay The new way primitive
          * @param tagCorrectionCommands The commands to correct the tags
          * @param reverseCommand The command to reverse the way
          */
-        public ReverseWayResult(Way newWay, Collection<Command> tagCorrectionCommands, Command reverseCommand) {
-            this.newWay = newWay;
+        public ReverseWayResult(Collection<Command> tagCorrectionCommands, Command reverseCommand) {
             this.tagCorrectionCommands = tagCorrectionCommands;
             this.reverseCommand = reverseCommand;
-        }
-
-        /**
-         * Gets the new way object
-         * @return The new, reversed way
-         */
-        public Way getNewWay() {
-            return newWay;
         }
 
         /**
@@ -149,16 +138,14 @@ public final class ReverseWayAction extends JosmAction {
      */
     public static ReverseWayResult reverseWay(Way w) throws UserCancelException {
         ReverseWayNoTagCorrector.checkAndConfirmReverseWay(w);
-        Way wnew = new Way(w);
-        List<Node> nodesCopy = wnew.getNodes();
+        List<Node> nodesCopy = w.getNodes();
         Collections.reverse(nodesCopy);
-        wnew.setNodes(nodesCopy);
 
         Collection<Command> corrCmds = Collections.<Command>emptyList();
         if (Config.getPref().getBoolean("tag-correction.reverse-way", true)) {
-            corrCmds = new ReverseWayTagCorrector().execute(w, wnew);
+            corrCmds = new ReverseWayTagCorrector().execute(w, w);
         }
-        return new ReverseWayResult(wnew, corrCmds, new ChangeCommand(w, wnew));
+        return new ReverseWayResult(corrCmds, new ChangeNodesCommand(w, new ArrayList<>(nodesCopy)));
     }
 
     @Override

src/org/openstreetmap/josm/command/ChangePropertyCommand.java
@@ -7,9 +7,11 @@ import static org.openstreetmap.josm.tools.I18n.trn;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -20,6 +22,7 @@ import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.DefaultNameFormatter;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
+import org.openstreetmap.josm.data.osm.Tagged;
 import org.openstreetmap.josm.tools.I18n;
 import org.openstreetmap.josm.tools.ImageProvider;
 
@@ -72,7 +75,7 @@ public class ChangePropertyCommand extends Command {
      *
      * @param ds The target data set. Must not be {@code null}
      * @param objects the objects to modify. Must not be empty
-     * @param tags the tags to set
+     * @param tags the tags to set. Caller must make sure that the tas are not changed once the command was executed.
      * @since 12726
      */
     public ChangePropertyCommand(DataSet ds, Collection<? extends OsmPrimitive> objects, Map<String, String> tags) {
@@ -85,7 +88,7 @@ public class ChangePropertyCommand extends Command {
      * Creates a command to change multiple tags of multiple objects
      *
      * @param objects the objects to modify. Must not be empty, and objects must belong to a data set
-     * @param tags the tags to set
+     * @param tags the tags to set. Caller must make sure that the tas are not changed once the command was executed.
      * @throws NullPointerException if objects is null or contain null item
      * @throws NoSuchElementException if objects is empty
      */
@@ -285,5 +288,36 @@ public class ChangePropertyCommand extends Command {
         ChangePropertyCommand that = (ChangePropertyCommand) obj;
         return Objects.equals(objects, that.objects) &&
                 Objects.equals(tags, that.tags);
+    }
+
+    /**
+     * Calculate the {@link ChangePropertyCommand} that is needed to change the tags in source to be equal to those in target.
+     * @param source the source primitive
+     * @param target the target primitive
+     * @return null if no changes are needed, else a {@link ChangePropertyCommand}
+     * @since 17357
+     */
+    public static Command build(OsmPrimitive source, Tagged target) {
+        Map<String, String> changedTags = new HashMap<>();
+        // find tags which have to be changed or removed
+        for (Entry<String, String> tag : source.getKeys().entrySet()) {
+            String key = tag.getKey();
+            String val = target.get(key);
+            if (!tag.getValue().equals(val))
+                changedTags.put(key, val); // null or a different value
+        }
+        // find tags which exist only in target, they have to be added
+        for (Entry<String, String> tag : target.getKeys().entrySet()) {
+            String key = tag.getKey();
+            if (!source.hasTag(key))
+                changedTags.put(key, tag.getValue());
+        }
+        if (changedTags.isEmpty())
+            return null;
+        if (changedTags.size() == 1) {
+            Entry<String, String> tag = changedTags.entrySet().iterator().next();
+            return new ChangePropertyCommand(Collections.singleton(source), tag.getKey(), tag.getValue());
+        }
+        return new ChangePropertyCommand(Collections.singleton(source), new HashMap<>(changedTags));
     }
 }

src/org/openstreetmap/josm/command/SplitWayCommand.java
@@ -16,6 +16,7 @@ import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -89,7 +90,7 @@ public class SplitWayCommand extends SequenceCommand {
      * @param commandList The sequence of commands that should be executed.
      * @param newSelection The new list of selected primitives ids (which is saved for later retrieval with {@link #getNewSelection})
      * @param originalWay The original way being split (which is saved for later retrieval with {@link #getOriginalWay})
-     * @param newWays The resulting new ways (which is saved for later retrieval with {@link #getOriginalWay})
+     * @param newWays The resulting new ways (which is saved for later retrieval with {@link #getNewWays})
      */
     public SplitWayCommand(String name, Collection<Command> commandList,
             List<? extends PrimitiveId> newSelection, Way originalWay, List<Way> newWays) {
@@ -401,20 +402,29 @@ public class SplitWayCommand extends SequenceCommand {
                     downloadMissingMembers(incompleteMembers);
                 } catch (OsmTransferException e) {
                     ExceptionDialogUtil.explainException(e);
+                    analysis.cleanup();
                     return Optional.empty();
                 }
                 // If missing relation members were downloaded, perform the analysis again to find the relation
                 // member order for all relations.
+                analysis.cleanup();
                 analysis = analyseSplit(way, wayToKeep, newWays);
-                return Optional.of(splitBasedOnAnalyses(way, newWays, newSelection, analysis, indexOfWayToKeep));
+                break;
             case GO_AHEAD_WITHOUT_DOWNLOADS:
                 // Proceed with the split with the information we have.
                 // This can mean that there are no missing members we want, or that the user chooses to continue
                 // the split without downloading them.
-                return Optional.of(splitBasedOnAnalyses(way, newWays, newSelection, analysis, indexOfWayToKeep));
+                break;
             case USER_ABORTED:
             default:
                 return Optional.empty();
+        }
+        try {
+            return Optional.of(splitBasedOnAnalyses(way, newWays, newSelection, analysis, indexOfWayToKeep));
+        } finally {
+            // see #19885
+            wayToKeep.setNodes(null);
+            analysis.cleanup();
         }
     }
 
@@ -464,7 +474,7 @@ public class SplitWayCommand extends SequenceCommand {
                         warnings.add(WarningType.GENERIC);
                     }
                     if (c == null) {
-                        c = new Relation(r);
+                        c = new Relation(r); // #19885: will be removed later
                     }
 
                     if (insert) {
@@ -551,10 +561,6 @@ public class SplitWayCommand extends SequenceCommand {
                     }
                 }
             }
-
-            if (c != null) {
-                commandList.add(new ChangeCommand(r.getDataSet(), r, c));
-            }
         }
         changedWay.setNodes(null); // see #19885
         return new Analysis(relationAnalyses, commandList, warnings, numberOfRelations);
@@ -574,6 +580,16 @@ public class SplitWayCommand extends SequenceCommand {
             commands = commandList;
             warningTypes = warnings;
             this.numberOfRelations = numberOfRelations;
+        }
+
+        /**
+         * Unlink temporary copies of relations. See #19885
+         */
+        void cleanup() {
+            for (RelationAnalysis ra : relationAnalyses) {
+                if (ra.relation.getDataSet() == null)
+                    ra.relation.setMembers(null);
+            }
         }
 
         List<RelationAnalysis> getRelationAnalyses() {
@@ -687,6 +703,7 @@ public class SplitWayCommand extends SequenceCommand {
             newSelection.addAll(newWays);
         }
 
+        Set<Relation> modifiedRelations = new LinkedHashSet<>();
         // Perform the split.
         for (RelationAnalysis relationAnalysis : analysis.getRelationAnalyses()) {
             RelationMember rm = relationAnalysis.getRelationMember();
@@ -728,6 +745,13 @@ public class SplitWayCommand extends SequenceCommand {
                     relation.addMember(j, em);
                 }
             }
+            modifiedRelations.add(relation);
+        }
+        for (Relation r : modifiedRelations) {
+            DataSet ds = way.getDataSet();
+            Relation orig = (Relation) ds.getPrimitiveById(r);
+            analysis.getCommands().add(new ChangeMembersCommand(orig, new ArrayList<>(r.getMembers())));
+            r.setMembers(null); // see #19885
         }
 
         EnumSet<WarningType> warnings = analysis.getWarningTypes();

src/org/openstreetmap/josm/data/osm/MultipolygonBuilder.java
@@ -117,15 +117,19 @@ public class MultipolygonBuilder {
     public String makeFromWays(Collection<Way> ways) {
         MultipolygonTest mpTest = new MultipolygonTest();
         Relation calculated = mpTest.makeFromWays(ways);
-        if (!mpTest.getErrors().isEmpty()) {
-            return mpTest.getErrors().iterator().next().getMessage();
+        try {
+            if (!mpTest.getErrors().isEmpty()) {
+                return mpTest.getErrors().iterator().next().getMessage();
+            }
+            Pair<List<JoinedPolygon>, List<JoinedPolygon>> outerInner = joinWays(calculated);
+            this.outerWays.clear();
+            this.innerWays.clear();
+            this.outerWays.addAll(outerInner.a);
+            this.innerWays.addAll(outerInner.b);
+            return null;
+        } finally {
+            calculated.setMembers(null); // see #19885
         }
-        Pair<List<JoinedPolygon>, List<JoinedPolygon>> outerInner = joinWays(calculated);
-        this.outerWays.clear();
-        this.innerWays.clear();
-        this.outerWays.addAll(outerInner.a);
-        this.innerWays.addAll(outerInner.b);
-        return null;
     }
 
     /**

src/org/openstreetmap/josm/gui/dialogs/properties/PropertiesDialog.java
@@ -1153,12 +1153,11 @@ implements DataSelectionListener, ActiveLayerChangeListener, DataSetListenerAdap
             if (ed.showDialog().getValue() != 1)
                 return;
 
-            Relation copy = new Relation(cur);
+            List<RelationMember> members = cur.getMembers();
             for (OsmPrimitive primitive: OsmDataManager.getInstance().getInProgressSelection()) {
-                copy.removeMembersFor(primitive);
+                members.removeIf(rm -> rm.getMember() == primitive);
             }
-            UndoRedoHandler.getInstance().add(new ChangeMembersCommand(cur, copy.getMembers()));
-            copy.setMembers(null); // see #19885
+            UndoRedoHandler.getInstance().add(new ChangeMembersCommand(cur, members));
 
             tagTable.clearSelection();
             if (nextRelation != null) {

commit 40485f867c0d6ccaa52550502df30d38a6d124b9

fix #20222: Relation editor: Changes in member order are lost when updates are applied - correct logic which decides what type of command is needed, regression from r17358
git-svn-id: https://josm.openstreetmap.de/svn/trunk@17401 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/relation/actions/SavingAction.java
@@ -91,7 +91,7 @@ abstract class SavingAction extends AbstractRelationEditorAction {
         tagEditorModel.applyToPrimitive(editedRelation);
         getMemberTableModel().applyToRelation(editedRelation);
         List<Command> cmds = new ArrayList<>();
-        if (originRelation.getKeys().equals(editedRelation.getKeys())) {
+        if (!originRelation.getMembers().equals(editedRelation.getMembers())) {
             cmds.add(new ChangeMembersCommand(originRelation, editedRelation.getMembers()));
         }
         Command cmdProps = ChangePropertyCommand.build(originRelation, editedRelation);